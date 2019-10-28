426db62edf34ac0fc8cefd76ef857b0db801566e
PepperDouglas <douglas.sebom@appliedtechnology.se>
2019-10-28 15:05:18 +0100

    added extract + insert + dropextract + avalanche

--- src/script.js
+++ src/script.js
@@ -1,12 +1,18 @@
 let currentSymbolChoice = '';
-
 let avaBtn = document.getElementById('avalanche_button');
-
+let extrButton = document.getElementById('extract');
+let insertButton = document.getElementById('insert');
+let exdropButton = document.getElementById('exdrop');
 let symbolMenu = document.getElementsByClassName('symbol_menu')[0].children;
-
 let boardArray = document.getElementsByClassName('board_row');
+let outField = document.getElementsByClassName('output_field');
+
 
 console.log(symbolMenu.innerText);
+avaBtn.addEventListener('click', fall);
+exdropButton.addEventListener('click', exportDrop);
+insertButton.addEventListener('click', insert);
+
 
 for(let i = 0; i < symbolMenu.length; i++){
     symbolMenu[i].addEventListener('click', function(t){
@@ -16,6 +22,7 @@ for(let i = 0; i < symbolMenu.length; i++){
     });
 
 }
+
 for(let i = 0; i < boardArray.length; i++){
     console.log('board rows');
     let aRow = boardArray[i];
@@ -27,7 +34,101 @@ for(let i = 0; i < boardArray.length; i++){
     }
 }
 
+//Id getter
+function intToStringId(num){
+    if (num < 10){
+        return 's0' + num;
+    }
+    return 's' + num;
+}
 
+let  afterAva = 0;
+//Avalanche the array
+function fall(){
+    let exArr = [];
+    let newArr = [];
+    let diff = 0;
+    for (let i = 0; i <= 24; i++){
+        const stringId = intToStringId(i);
+        const selectedElement = document.getElementById(stringId);
+        const positionSym = selectedElement.innerText;
+        exArr.push(positionSym);
+    }
+    newArr = exArr.filter(e => e !== 'NONE');
+    diff = 25 - newArr.length;
+    afterAva = diff;
+    for (let i = 0; i < diff; i++){
+        newArr.unshift('');
+    }
+    for (let i = 0; i <= 24; i++){
+        const stringId = intToStringId(i);
+        document.getElementById(stringId).innerText = newArr[i];
+    }
+}
 
+//Convert array to jiemeks string
+function stringFromArr(arr){
+    let jmxCode = '';
+    for (let i = 0; i < arr.length - 1; i++){
+        let stringNoSpaceSplit = arr[i].split(' ');
+        let stringNoSpaceJoined = stringNoSpaceSplit[0] + stringNoSpaceSplit[1];
+        if (arr[i + 1] !== undefined){
+            stringNoSpaceJoined += ',';
+        }
+        jmxCode += stringNoSpaceJoined;
+    }
+    jmxCode += '\n';
+    return jmxCode;
+}
 
+const displayArr = [];
+//function Export DROP
+function exportDrop(){
+    let extractedArr = [];
+    for (let i = 0; i <= afterAva; i++){
+        const stringId = intToStringId(i);
+        const selectedElement = document.getElementById(stringId);
+        const positionSym = selectedElement.innerText;
+        extractedArr.push(positionSym);
+    }
+    outField[0].innerText += stringFromArr(extractedArr);
+}
 
+//function Export Position
+extrButton.addEventListener('click', exportPosition);
+
+function exportPosition(){
+    let extractedArr = [];
+    for (let i = 0; i <= 24; i++){
+        const stringId = intToStringId(i);
+        const selectedElement = document.getElementById(stringId);
+        const positionSym = selectedElement.innerText;
+        extractedArr.push(positionSym);
+    }
+    outField[0].innerText += stringFromArr(extractedArr);
+}
+
+//showcase according to JMX string
+function insert(){
+    const insText = document.getElementById('insertText').value;
+    let insArr = arrFromString(insText);
+    for (let i = 0; i < insArr.length; i++){
+        const stringId = intToStringId(i);
+        document.getElementById(stringId).innerText = insArr[i];
+    }
+}
+
+//array from a string
+function arrFromString(str){
+    str = str.split(',');
+    str.filter(e => e !== ',');
+    str.pop();
+    let newArr = [];
+    for (let i = 0; i < str.length; i++){
+        let arr = str[i].split('SYM');
+        arr[0] = 'SYM';
+        let arr2 = arr.join(' ')
+        newArr.push(arr2);
+    }
+    return newArr;
+}
